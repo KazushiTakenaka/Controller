9bd77189ece4dd4d4fc1afff1e762d3beca791b4
take <take.kazushi@gmail.com>
2024-11-22 11:34:39 +0900

    add switch

--- src/main.cpp
+++ src/main.cpp
@@ -17,13 +17,13 @@ const int SW5 = 14;
 const int SW6 = 25;
 const int SW7 = 26;
 const int SW8 = 27;
-const int SLD_SW1 = 32;
+const int SLD_SW1_1 = 32;
 const int SLD_SW1_2 = 2;
-const int SLD_SW2 = 33;
+const int SLD_SW2_1 = 33;
 const int SLD_SW2_2 = 4;
-const int SLD_SW3 = 22;
+const int SLD_SW3_1 = 22;
 const int SLD_SW3_2 = 5;
-const int SLD_SW4 = 23;
+const int SLD_SW4_1 = 23;
 const int SLD_SW4_2 = 19;
 const int BLE_LED = 21;
 bool connected;
@@ -52,13 +52,13 @@ void setup() {
   pinMode(SW6, INPUT_PULLUP);
   pinMode(SW7, INPUT_PULLUP);
   pinMode(SW8, INPUT_PULLUP);
-  pinMode(SLD_SW1, INPUT_PULLUP);
-  pinMode(SLD_SW2, INPUT_PULLUP);
-  pinMode(SLD_SW3, INPUT_PULLUP);
-  pinMode(SLD_SW4, INPUT_PULLUP);
+  pinMode(SLD_SW1_1, INPUT_PULLUP);
   pinMode(SLD_SW1_2, INPUT_PULLUP);
+  pinMode(SLD_SW2_1, INPUT_PULLUP);
   pinMode(SLD_SW2_2, INPUT_PULLUP);
+  pinMode(SLD_SW3_1, INPUT_PULLUP);
   pinMode(SLD_SW3_2, INPUT_PULLUP);
+  pinMode(SLD_SW4_1, INPUT_PULLUP);
   pinMode(SLD_SW4_2, INPUT_PULLUP);
   pinMode(ANALOG_READ1, INPUT);
   pinMode(ANALOG_READ2, INPUT);
@@ -76,10 +76,14 @@ struct SendData
   {
     int slideVal1;
     int slideVal2;
-    int sld_sw1;
-    int sld_sw2;
-    int sld_sw3;
-    int sld_sw4;
+    int sld_sw1_1;
+    int sld_sw1_2;
+    int sld_sw2_1;
+    int sld_sw2_2;
+    int sld_sw3_1;
+    int sld_sw3_2;
+    int sld_sw4_1;
+    int sld_sw4_2;
     int sw1;
     int sw2;
     int sw3;
@@ -92,27 +96,11 @@ struct SendData
 
 SendData dataLoading();
 
-/*テスト*/
-int sld_sw1_2;
-int sld_sw2_2;
-int sld_sw3_2;
-int sld_sw4_2;
-
 void loop() {
   /*構造体に送信情報を代入*/
   SendData sendData;
   sendData = dataLoading();
   SerialBT.write((uint8_t*)&sendData, sizeof(sendData));// 構造体送信
-
-  /*テスト*/
-  sld_sw1_2 = digitalRead(SLD_SW1_2);
-  sld_sw2_2 = digitalRead(SLD_SW2_2);
-  sld_sw3_2 = digitalRead(SLD_SW3_2);
-  sld_sw4_2 = digitalRead(SLD_SW4_2);
-  Serial.print(sld_sw1_2);
-  Serial.print(sld_sw2_2);
-  Serial.print(sld_sw3_2);
-  Serial.println(sld_sw4_2);
 
   /*通信途切れで再起動*/
   if (SerialBT.available()) {
@@ -143,10 +131,14 @@ SendData dataLoading() {
   SendData readDatas;
   readDatas.slideVal1 = map(slideVal1, 0, 4095, 0, 255);
   readDatas.slideVal2 = map(slideVal2, 0, 4095, 0, 255);
-  readDatas.sld_sw1 = digitalRead(SLD_SW1);
-  readDatas.sld_sw2 = digitalRead(SLD_SW2);
-  readDatas.sld_sw3 = digitalRead(SLD_SW3);
-  readDatas.sld_sw4 = digitalRead(SLD_SW4);
+  readDatas.sld_sw1_1 = digitalRead(SLD_SW1_1);
+  readDatas.sld_sw1_2 = digitalRead(SLD_SW1_2);
+  readDatas.sld_sw2_1 = digitalRead(SLD_SW2_1);
+  readDatas.sld_sw2_2 = digitalRead(SLD_SW2_2);
+  readDatas.sld_sw3_1 = digitalRead(SLD_SW3_1);
+  readDatas.sld_sw3_2 = digitalRead(SLD_SW3_2);
+  readDatas.sld_sw4_1 = digitalRead(SLD_SW4_1);
+  readDatas.sld_sw4_2 = digitalRead(SLD_SW4_2);
   readDatas.sw1 = digitalRead(SW1);
   readDatas.sw2 = digitalRead(SW2);
   readDatas.sw3 = digitalRead(SW3);
@@ -155,11 +147,15 @@ SendData dataLoading() {
   readDatas.sw6 = digitalRead(SW6);
   readDatas.sw7 = digitalRead(SW7);
   readDatas.sw8 = digitalRead(SW8);
-  #if 0
-  Serial.print(readDatas.sld_sw1);
-  Serial.print(readDatas.sld_sw2);
-  Serial.print(readDatas.sld_sw3);
-  Serial.print(readDatas.sld_sw4);
+  #if 1
+  Serial.print(readDatas.sld_sw1_1);
+  Serial.print(readDatas.sld_sw1_2);
+  Serial.print(readDatas.sld_sw2_1);
+  Serial.print(readDatas.sld_sw2_2);
+  Serial.print(readDatas.sld_sw3_1);
+  Serial.print(readDatas.sld_sw3_2);
+  Serial.print(readDatas.sld_sw4_1);
+  Serial.print(readDatas.sld_sw4_2);
   Serial.print(readDatas.sw1);
   Serial.print(readDatas.sw2);
   Serial.print(readDatas.sw3);
